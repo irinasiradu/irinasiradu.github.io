399b7ddbf9132b4938e9e9b1246fbcea3260de24
Radu Lacatus <radu.lacatus@cegeka.com>
2020-06-01 15:09:57 +0300

    unskip translation test

--- src/shared/common.test.js
+++ src/shared/common.test.js
@@ -4,8 +4,11 @@ it('should have all translations', () => {
     expect(Object.keys(localizations.ro).sort()).toEqual(Object.keys(localizations.fr).sort());
 });
 
-it.skip('should not contain copy paste errors in translations', () => {
-    Object.keys(localizations.ro).forEach(x => {
-        expect(localizations.ro[x]).not.toEqual(localizations.fr[x]);
-    })
+const exceptions = ["Email"];
+it('should not contain copy paste errors in translations', () => {
+    Object.keys(localizations.ro)
+        .filter(x => exceptions.indexOf(x) > -1)
+        .forEach(x => {
+            expect(localizations.ro[x]).not.toEqual(localizations.fr[x]);
+        })
 })
